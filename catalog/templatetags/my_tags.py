e159aa152e5266b97ba7801e6d2e1c2fb97013bc
ocean88 <oceann3@gmail.com>
2024-03-28 20:50:18 +0900

    исправлена ошибка с загрузкой json

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -7,11 +7,9 @@ register = template.Library()
 def my_media_filter(data):
     if data:
         return f"/media/{data}"
-    return '#'
 
 
 @register.simple_tag
 def my_media_tag(data):
     if data:
         return f"/media/{data}"
-    return '#'
